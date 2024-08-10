43406ca854f69dc679abd227d94fec6d60bd5234
kimzeze <fridaynight@kakao.com>
2024-08-10 20:54:53 +0900

    [🗼markup] 메인페이지 테스트 마크업

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,10 @@
+import React from 'react';
+import Title from '@/components/Title';
+
 export default function Home() {
-  return <main className="border"></main>;
+  return (
+    <main className="border">
+      <Title title="Notice" href="/" />
+    </main>
+  );
 }
