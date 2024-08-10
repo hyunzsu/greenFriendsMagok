123d659a959930bf68f26cdb0216376ee2920d5a
kimzeze <fridaynight@kakao.com>
2024-08-10 22:16:34 +0900

    [🗼markup] 루트레이아웃 접근성을 위한 h1 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,16 +4,14 @@ import Footer from '@/components/Footer';
 import { Analytics } from '@vercel/analytics/react';
 import './globals.css';
 
-/* 메타데이터 설정 */
 export const metadata: Metadata = {
   title: '녹색친구들 마곡',
   description: '녹색친구들 마곡 입주예정자들을 위한 정보 공유 사이트',
 };
 
-/* 뷰포트 설정 */
 export const viewport: Viewport = {
-  width: 'device-width', // 모바일 기기의 화면 크기와 맞춤
-  initialScale: 1, // 초기 화면 배율
+  width: 'device-width',
+  initialScale: 1,
 };
 
 export default function RootLayout({
@@ -25,7 +23,10 @@ export default function RootLayout({
     <html lang="ko">
       <body className="flex min-h-screen flex-col text-base antialiased">
         <Header />
-        <main className="flex-grow px-4 py-6">{children}</main>
+        <main className="flex-grow px-4 py-6">
+          <h1 className="sr-only">녹색친구들 마곡</h1>
+          {children}
+        </main>
         <div id="portal"></div>
         <Footer />
         <Analytics />
