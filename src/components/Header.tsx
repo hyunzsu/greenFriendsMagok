4fd6531001082124ad0b0ccbae45177073c377ba
kimzeze <fridaynight@kakao.com>
2024-08-10 19:32:18 +0900

    [🗼markup] Header 마크업 구조 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,41 +3,14 @@ import Image from 'next/image';
 
 export default function Header() {
   return (
-    <header className="border-primary text-primary w-full border-b bg-white text-sm">
-      <div className="mx-auto flex max-w-screen-xl flex-col justify-start p-4 md:flex-row md:items-center md:justify-between">
-        <div className="mb-2 border md:mb-0">
-          <Image
-            className="flex items-center justify-center"
-            src="/assets/Logo.svg"
-            alt="녹색친구들 마곡 로고 이미지"
-            width={244}
-            height={25}
-          />
+    <header className="border-primary text-primary w-full border-b bg-white px-2.5">
+      <h1 className="sr-only">녹색친구들 마곡</h1>
+      <div className="mx-auto flex h-[50px] max-w-screen-xl justify-start md:flex-row md:items-center md:justify-between">
+        <div className="flex items-center justify-center border md:mb-0">
+          <span>
+            <Image src="/assets/Logo.svg" alt="녹색친구들 마곡 로고 이미지" width={244} height={25} priority />
+          </span>
         </div>
-        <nav>
-          <ul className="flex flex-wrap justify-start space-x-4">
-            <li>
-              <a href="#" className="hover:underline">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
-                Services
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </nav>
       </div>
     </header>
   );
