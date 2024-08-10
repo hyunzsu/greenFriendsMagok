a0e37b2eec699b6493a196f8bfa7e20cb6662caf
kimzeze <fridaynight@kakao.com>
2024-08-10 18:58:42 +0900

    [🧹chore] prettier 순서 수정

--- src/lib/hooks/useModal.tsx
+++ src/lib/hooks/useModal.tsx
@@ -2,7 +2,7 @@
 
 import { createPortal } from 'react-dom';
 import React, { useState, useEffect, useRef } from 'react';
-import Image from 'next/image';
+// import Image from 'next/image';
 
 type TModalPortal = {
   children: React.ReactNode;
@@ -42,7 +42,7 @@ export default function useModal() {
         // 모달 컨테이너 스타일
         <div className="fixed left-0 top-0 z-[99] flex h-full w-full items-center justify-center bg-black bg-opacity-50">
           <div>
-            <div className="flex h-[38px] items-end justify-between border-b border-primary bg-white">
+            <div className="border-primary flex h-[38px] items-end justify-between border-b bg-white">
               <div className="grow"></div>
               <div className="flex w-[48px] cursor-pointer items-center justify-center" onClick={closeModal}></div>
             </div>
