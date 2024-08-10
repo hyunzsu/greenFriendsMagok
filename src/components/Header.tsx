c16627235eb8646ce69612cb1d22e2b26faf515b
kimzeze <fridaynight@kakao.com>
2024-08-10 22:17:59 +0900

    [✨feat] Header에 메뉴 기능 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
+import MeatBallsMenu from './MeatBallsMenu';
 
-export default function Header() {
+const Header = () => {
   return (
-    <header className="border-primary text-primary w-full border-b bg-white px-2.5">
-      <h1 className="sr-only">녹색친구들 마곡</h1>
-      <div className="mx-auto flex h-[50px] max-w-screen-xl justify-start md:flex-row md:items-center md:justify-between">
-        <div className="flex items-center justify-center border md:mb-0">
-          <span>
-            <Image src="/assets/Logo.svg" alt="녹색친구들 마곡 로고 이미지" width={244} height={25} priority />
-          </span>
+    <header className="w-full border-b border-primary bg-white px-2.5 text-primary">
+      <div className="mx-auto flex h-[50px] max-w-screen-xl items-center justify-between">
+        <div className="flex items-center">
+          <Link href="/" aria-label="홈으로 이동">
+            <div className="pt-[4px]">
+              <Image src="/assets/Logo.svg" alt="녹색친구들 마곡 로고" width={244} height={25} priority />
+            </div>
+          </Link>
         </div>
+        <MeatBallsMenu />
       </div>
     </header>
   );
-}
+};
+
+export default Header;
