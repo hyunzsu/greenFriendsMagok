5486741a1d95a5ad08424879b58bfe83918bcf81
kimzeze <fridaynight@kakao.com>
2024-08-11 14:16:47 +0900

    [🧹chore] index에 컴포넌트 추가

--- src/components/_index.ts
+++ src/components/_index.ts
@@ -3,3 +3,6 @@ export { default as Footer } from './Footer';
 export { default as Button } from './Button';
 export { default as Title } from './Title';
 export { default as MeatBallsMenu } from './MeatBallsMenu';
+export { default as MenuModal } from './MenuModal';
+export { default as CategoryButton } from './CategoryButton';
+export { default as SlideCategory } from './SlideCategory';
