4a5f6713a6686d9ccb63440bad4694513708bd22
kimzeze <fridaynight@kakao.com>
2024-08-11 15:03:20 +0900

    [⚙setting] 카테고리 데이터, 타입 파일 분리하여 추가

---
+++ src/lib/data/categories.ts
@@ -0,0 +1,5 @@
+// Community 카테고리 데이터
+export const communityCategories = ['전체', '잡담', '추천해요', '나눔', '후기', 'Q&A', '함께해요', '도와주세요'];
+
+// Notice 카테고리 데이터
+export const noticeCategories = ['전체', '알립니다', '입주', 'Event'];
