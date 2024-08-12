c323e92c646bba0b784e4e98df90bdb4002d17d1
kimzeze <fridaynight@kakao.com>
2024-08-13 01:00:04 +0900

    [🔰type] 데이터 베이스의 테이블 타입 정의

---
+++ src/lib/types/database.ts
@@ -0,0 +1,29 @@
+/* 기본 타입 */
+export interface BaseRecord {
+  id: number;
+  created_at: string;
+}
+
+/* 공지사항 게시글 타입 */
+export interface NoticeTable extends BaseRecord {
+  title: string;
+  content: string;
+  author: string;
+}
+
+/* Forum 게시글 타입 */
+export interface ForumPostTable extends BaseRecord {
+  title: string;
+  content: string;
+  author: string;
+}
+
+/* 테이블 이름 정의 */
+export type TableName = 'notices' | 'forum_posts';
+
+/* 테이블별 레코드 타입 */
+export type RecordType<T extends TableName> = T extends 'notices'
+  ? NoticeTable
+  : T extends 'forum_posts'
+    ? ForumPostTable
+    : never;
