f528f56ae0b28ae551e5e795cb070a1bfc1ab97e
kimzeze <fridaynight@kakao.com>
2024-08-13 01:02:52 +0900

    [🧹chore] 타입 경로 수정

--- src/app/notice/_components/NoticeBoard.tsx
+++ src/app/notice/_components/NoticeBoard.tsx
@@ -1,9 +1,9 @@
 import { getNotices } from '../_lib/noticeService';
-import { NoticeContent } from '@/lib/types/notice';
+import { NoticeTable } from '@/lib/types/database';
 import NoticeItem from './NoticeItem';
 
 export default async function NoticeBoard() {
-  const notices: NoticeContent[] = await getNotices();
+  const notices: NoticeTable[] = await getNotices();
 
   return (
     <div className="my-2 space-y-4 border border-primary">
