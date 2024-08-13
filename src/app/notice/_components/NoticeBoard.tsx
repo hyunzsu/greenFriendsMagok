bbafd2fd88769458225a017b34ffa56fff32a376
kimzeze <fridaynight@kakao.com>
2024-08-13 14:21:54 +0900

    [💄style] 공지사항 테두리 및 여백 스타일 수정

--- src/app/notice/_components/NoticeBoard.tsx
+++ src/app/notice/_components/NoticeBoard.tsx
@@ -6,7 +6,7 @@ export default async function NoticeBoard() {
   const notices: NoticeTable[] = await getNotices();
 
   return (
-    <div className="my-2 flex flex-col border border-primary">
+    <div className="my-5 flex flex-col">
       {notices.map((notice) => (
         <NoticeItem key={notice.id} notice={notice} />
       ))}
