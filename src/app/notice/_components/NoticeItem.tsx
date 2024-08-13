0cff30d9f3133822a3ae81bed81fb16e34a99e9a
kimzeze <fridaynight@kakao.com>
2024-08-13 19:07:01 +0900

    [🗼markup] 공지사항 제목 태그를 h2에서 p로 변경

--- src/app/notice/_components/NoticeItem.tsx
+++ src/app/notice/_components/NoticeItem.tsx
@@ -11,7 +11,7 @@ export default function NoticeItem({ notice }: NoticeItemProps) {
   return (
     <Link href={`/notice/${notice.id}`}>
       <div className="cursor-pointer border-t border-primary p-2 active:bg-gray-50">
-        <h2 className="text-14 font-semibold">{notice.title}</h2>
+        <p className="text-14 font-semibold">{notice.title}</p>
         <div className="mt-1 flex flex-row justify-between text-12 text-gray-500">
           <p>{new Date(notice.created_at).toLocaleDateString()}</p>
           <p>{notice.author}</p>
