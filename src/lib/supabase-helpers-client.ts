5612a4c771c54f0edc0b7f31cd0692e11e43f801
kimzeze <fridaynight@kakao.com>
2024-08-14 00:32:58 +0900

    [✨feat] supabase-client 함수에서 카테고리 게시글 가져올 때 날짜순으로 순서 정렬

--- src/lib/supabase-helpers-client.ts
+++ src/lib/supabase-helpers-client.ts
@@ -1,18 +1,6 @@
 import { supabase } from './supabase';
 import { TableName, RecordType } from '@/lib/types/database';
 
-// 데이터베이스 연결을 확인하는 함수
-async function checkDatabaseConnection() {
-  try {
-    const { data, error } = await supabase.from('notices').select('count', { count: 'exact' });
-    if (error) throw error;
-    console.log('데이터베이스 연결 성공. 행 수:', data);
-  } catch (error) {
-    console.error('데이터베이스 연결 실패:', error);
-    throw error;
-  }
-}
-
 // 클라이언트에서 특정 테이블의 모든 데이터를 가져오는 함수
 export async function fetchAllFromTableClient<T extends TableName>(
   tableName: T,
@@ -48,11 +36,8 @@ export async function fetchFilteredFromTable<T extends TableName>(
   category: string,
   columns: string = '*',
 ): Promise<RecordType<T>[]> {
-  console.log('테이블에서 가져오는 중: ', tableName, '카테고리:', category);
-  await checkDatabaseConnection();
-
   // 카테고리 필터링 적용
-  let query = supabase.from(tableName).select(columns);
+  let query = supabase.from(tableName).select(columns).order('created_at', { ascending: false });
 
   // 카테고리가 전체가 아닐경우 카테고리 필터링 적용
   if (category !== '전체') {
