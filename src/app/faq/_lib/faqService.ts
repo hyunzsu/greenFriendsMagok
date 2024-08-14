e0d3bcb88afbe0e13efe2b62717df713fb89ff77
hyunzsu <hyunzsu@naver.com>
2024-08-14 22:36:44 +0900

    [✨feat] supabase에서 faqs 테이블 데이터를 가져오는 함수 개발

---
+++ src/app/faq/_lib/faqService.ts
@@ -0,0 +1,16 @@
+import { createServerSupabaseClient } from '@/lib/supabase-server';
+import { FAQTable } from '@/lib/types/database';
+
+export async function getFaqs(): Promise<FAQTable[]> {
+  const supabase = createServerSupabaseClient();
+
+  const { data, error } = await supabase.from('faqs').select('*').order('id', { ascending: true });
+
+  if (error) {
+    console.error('FAQ에서 데이터를 가져오는 중 오류 발생:', error);
+    return [];
+  }
+
+  console.log(data);
+  return data as FAQTable[];
+}
