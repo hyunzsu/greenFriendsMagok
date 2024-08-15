f914879c34b1c6a8c73224802887ba56d9570bdb
kimzeze <fridaynight@kakao.com>
2024-08-16 03:19:54 +0900

    [✨feat] 제목을 누르면 해당 페이지 가도록 수정

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -8,33 +8,19 @@ interface TitleProps {
 }
 
 export default function Title({ title, href, isMainPage }: TitleProps) {
-  const TitleContent = () => (
-    <div className="flex cursor-pointer flex-row items-center justify-between py-2">
-      <h3 className={cn('font-semibold', isMainPage ? 'text-2xl' : 'mb-3 text-3xl')}>{title}</h3>
-      {isMainPage && (
-        <div className="ml-8 flex flex-row items-center">
-          <span className="text-xs">더보기</span>
-          <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M5.1333 12.1334L10.2666 7.00003L5.1333 1.8667" stroke="#6E6E6E" strokeMiterlimit="10" />
-          </svg>
-        </div>
-      )}
-    </div>
-  );
-
-  // 메인 페이지인 경우 Link 컴포넌트로 감싸서 반환
-  if (isMainPage) {
-    return (
-      <Link href={href} className="block">
-        <TitleContent />
-      </Link>
-    );
-  }
-
-  // 메인 페이지가 아닌 경우 div로 감싸서 반환
   return (
-    <div className="block">
-      <TitleContent />
-    </div>
+    <Link href={href} className="block">
+      <div className="flex cursor-pointer flex-row items-center justify-between py-2">
+        <h3 className={cn('font-semibold', isMainPage ? 'text-2xl' : 'mb-3 text-3xl')}>{title}</h3>
+        {isMainPage && (
+          <div className="ml-8 flex flex-row items-center">
+            <span className="text-xs">더보기</span>
+            <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M5.1333 12.1334L10.2666 7.00003L5.1333 1.8667" stroke="#6E6E6E" strokeMiterlimit="10" />
+            </svg>
+          </div>
+        )}
+      </div>
+    </Link>
   );
 }
