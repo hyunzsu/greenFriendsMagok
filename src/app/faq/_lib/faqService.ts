8051e468b5bd3019a17d2d3a7a046af9e416e690
kimzeze <fridaynight@kakao.com>
2024-08-16 00:07:27 +0900

    [✨feat] FAQ에서 테이블이름을 전달하여 데이터를 받을 수 있도록 기능 수정

--- src/app/faq/_lib/faqService.ts
+++ src/app/faq/_lib/faqService.ts
@@ -1,10 +1,10 @@
 import { createServerSupabaseClient } from '@/lib/supabase-server';
 import { FAQTable } from '@/lib/types/database';
 
-export async function getFaqs(): Promise<FAQTable[]> {
+export async function getFaqs({ tableName }: { tableName: string }): Promise<FAQTable[]> {
   const supabase = createServerSupabaseClient();
 
-  const { data, error } = await supabase.from('faqs').select('*').order('id', { ascending: true });
+  const { data, error } = await supabase.from(tableName).select('*').order('id', { ascending: true });
 
   if (error) {
     console.error('FAQ에서 데이터를 가져오는 중 오류 발생:', error);
