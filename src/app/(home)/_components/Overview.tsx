e4b97728c5f955536dde53e350ee3f75fecf8d1d
kimzeze <fridaynight@kakao.com>
2024-08-16 05:11:38 +0900

    [🗼markup] Overview 컴포넌트 마크업

---
+++ src/app/(home)/_components/Overview.tsx
@@ -0,0 +1,8 @@
+export default function Overview() {
+  return (
+    <div className="flex cursor-pointer flex-col items-center justify-between py-2">
+      <h3 className="mb-3 w-full text-left text-2xl font-semibold">Overview</h3>
+      <div className="h-[500px] w-full bg-tertiary"></div>
+    </div>
+  );
+}
