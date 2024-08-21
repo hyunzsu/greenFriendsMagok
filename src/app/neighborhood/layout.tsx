dd066294ce34d57841bbf995d7662f0e43a229e4
kimzeze <fridaynight@kakao.com>
2024-08-21 22:56:00 +0900

    [🗼markup] 주변 정보를 알려주는  Neighborhood 페이지 및 라우터 추가

---
+++ src/app/neighborhood/layout.tsx
@@ -0,0 +1,5 @@
+export const revalidate = 60; // 60초마다 재검증
+
+export default function NeighborhoodLayout({ children }: { children: React.ReactNode }) {
+  return <div className="px-4 pb-6 pt-4">{children}</div>;
+}
