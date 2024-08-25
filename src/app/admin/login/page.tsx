ed86067c71ce729715bfe50f198a7a16f6ea9239
hyunzsu <hyunzsu@naver.com>
2024-08-25 14:47:15 +0900

    [🗼markup] 관리자 로그인 페이지 마크업

---
+++ src/app/admin/login/page.tsx
@@ -0,0 +1,10 @@
+import AdminLogin from './_components/AdminLogin';
+
+export default function page() {
+  return (
+    <section>
+      <h2 className="my-14 text-center text-3xl font-bold">관리자 로그인</h2>
+      <AdminLogin />
+    </section>
+  );
+}
