11b978fb10c9d3f8c048f08c64f9d866d08f7cf9
hyunzsu <hyunzsu@naver.com>
2024-08-25 18:38:38 +0900

    [🗼markup] 로그인 페이지 생성

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+import LoginForm from './_components/LoginForm';
+
+export default function page() {
+  return (
+    <section>
+      <h2 className="my-14 text-center text-3xl font-bold">로그인</h2>
+      <LoginForm />
+    </section>
+  );
+}
