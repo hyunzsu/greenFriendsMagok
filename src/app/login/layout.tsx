9069b635d8d8bb1a06b0f9449500ab804d65322e
hyunzsu <hyunzsu@naver.com>
2024-09-29 16:30:27 +0900

    [🗼markup] 로그인 페이지 생성 및 기존 로그인 코드 제거

--- src/app/login/layout.tsx
+++
@@ -1,5 +0,0 @@
-export const revalidate = 60; // 60초마다 재검증
-
-export default function SignupLayout({ children }: { children: React.ReactNode }) {
-  return <div className="px-4 pb-6 pt-4">{children}</div>;
-}
