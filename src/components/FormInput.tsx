18b5b0065e4c39ca1ae2d23b2e8ba7fab60c50ac
hyunzsu <hyunzsu@naver.com>
2024-09-30 18:38:51 +0900

    [💄style] FormInput 체크박스 커스텀 스타일링

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -1,4 +1,5 @@
 import { FieldError, UseFormRegister } from 'react-hook-form';
+import Image from 'next/image';
 
 type FormInputProps = {
   label: string;
@@ -8,6 +9,7 @@ type FormInputProps = {
   type?: string;
   placeholder?: string;
   isCheckbox?: boolean;
+  width?: string;
 };
 
 export default function FormInput({
@@ -18,21 +20,41 @@ export default function FormInput({
   type = 'text',
   placeholder = '',
   isCheckbox = false,
+  width = 'w-full',
 }: FormInputProps) {
   return (
-    <div>
+    <div className={width}>
       {isCheckbox ? (
-        <div>
-          <input id={name} type="checkbox" {...register(name)} />
-          <label htmlFor={name}>{label}</label>
-        </div>
+        <label htmlFor={name} className="flex cursor-pointer items-center gap-1">
+          <input id={name} type="checkbox" {...register(name)} className="sr-only" />
+          <span className="checkbox-icon">
+            <svg width="21" height="25" viewBox="0 0 21 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M8.2539 18.0292L3.9585 12.8521L4.57558 12.1094L8.2539 16.5427L16.1671 7.00522L16.7834 7.74793L8.2539 18.0292Z"
+                fill="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </span>
+          <span className="text-sm font-medium">{label}</span>
+        </label>
       ) : (
-        <div className="flex w-full flex-col">
-          <label htmlFor={name}>{label}</label>
-          <input id={name} type={type} placeholder={placeholder} {...register(name)} />
+        <div className="flex w-full flex-col gap-2 text-sm">
+          <label htmlFor={name} className="font-bold">
+            {label}
+          </label>
+          <input
+            id={name}
+            type={type}
+            placeholder={placeholder}
+            {...register(name)}
+            className="border-b border-primary transition-colors duration-200 ease-in-out hover:border-tertiary focus:border-tertiary focus:outline-none"
+          />
         </div>
       )}
-      {error && <p>{error.message}</p>}
+      {error && <p className="mt-1 text-xs text-warning">{error.message}</p>}
     </div>
   );
 }
